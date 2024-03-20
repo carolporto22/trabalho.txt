82300665101248a5aa67d5ebd89c822362eb3079
carolporto22 <161602767+carolporto22@users.noreply.github.com>
2024-03-20 09:34:50 -0300

    Add files via upload

---
+++ semana 06.py
@@ -0,0 +1,57 @@
+print("carol")
+numero = 10
+nome = "João"
+booleano = True
+a = 10
+b = 5
+soma = a + b
+subtraçao = a - b
+multiplicaçao = a * b
+divisao = a / b
+numero1 = 10
+numero2 = 20
+numero3 = 30
+numero4 = 40
+
+frase = "Meu nome é Carol."
+
+palavra = "programação"
+
+
+numeros = [10, 20, 30, 40]
+palavra = "programação"
+
+
+media = sum(numeros) / len(numeros)
+print("Média aritmética dos números:", media)
+
+
+num_quadrado = numeros[0]  
+quadrado = num_quadrado ** 2
+print("Quadrado de", num_quadrado, ":", quadrado)
+
+
+num_dobro = numeros[1] 
+dobro = num_dobro * 2
+print("Dobro de", num_dobro, ":", dobro)
+
+
+quantidade_letras = len(palavra)
+print("Quantidade de letras na palavra:", quantidade_letras)
+
+
+quantidade_espacos = palavra.count(' ')
+print("Quantidade de espaços em branco na frase:", quantidade_espacos)
+
+
+primeiro_numero = numeros[0]
+segundo_numero = numeros[1]
+if primeiro_numero > segundo_numero:
+    print("O primeiro número é maior que o segundo.")
+else:
+    print("O primeiro número não é maior que o segundo.")
+
+
+maior_numero = max(numeros)
+print("O maior número é:", maior_numero)
+
